2af6f0c0c316ab914f710d3909dfcf90589679f0
AliAghaahmadi <itsaliaghaahmadi@gmail.com>
2025-03-18 20:50:22 +0330

    some fixes

--- brocproc/src/lib.rs
+++ brocproc/src/lib.rs
@@ -1,15 +1,49 @@
 mod parse;
 mod xformat;
-use xformat::*;
-
-use crate::parse::{MacroArgs};
+use quote::quote;
 use syn::parse_macro_input;
+use xformat::*;
+use crate::parse::MacroArgs;
 
 #[proc_macro]
-pub fn test_proc(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn xprintln(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let MacroArgs { args } = parse_macro_input!(input as MacroArgs);
+    let out : proc_macro2::TokenStream = xformat_args(&args).into();
 
-    let out = xformat_block(&args, 0);
+    quote! {
+        println!("{}", #out)
+    }.into()
+}
 
-    out
+#[proc_macro]
+pub fn xprint(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let MacroArgs { args } = parse_macro_input!(input as MacroArgs);
+    let out : proc_macro2::TokenStream = xformat_args(&args).into();
+
+    quote! {
+        print!("{}", #out)
+    }.into()
+}
+
+#[proc_macro]
+pub fn xeprintln(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let MacroArgs { args } = parse_macro_input!(input as MacroArgs);
+    let out : proc_macro2::TokenStream = xformat_args(&args).into();
+
+
+    quote! {{
+        use broccolor::TextStyle;
+
+        println!("{} {}", "error:".term_colorize(Color::Red), #out)
+    }}.into()
+}
+
+#[proc_macro]
+pub fn xprintb(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let MacroArgs { args } = parse_macro_input!(input as MacroArgs);
+    let out : proc_macro2::TokenStream = xformat_block(&args, 0).into();
+
+    quote! {
+        println!("{}", #out)
+    }.into()
 }
